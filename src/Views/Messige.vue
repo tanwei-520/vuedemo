8b6d3d5e1ddd96acca39b5f7b7866fdf15f6d189
twhome <1780382649@qq.com>
2021-09-11 23:13:13 +0800

    编程式导航

--- src/Views/Messige.vue
+++ src/Views/Messige.vue
@@ -3,14 +3,21 @@
     <h4>镶嵌路由</h4>
     <ul>
         <li v-for="itrem in items" :key="itrem.id"><router-link :to="'/news/messige/list/'+itrem.id">{{itrem.text}}</router-link>
-        <router-link :to="'/news/messige/list/?id='+itrem.id">{{itrem.text}} query</router-link></li>
+        <router-link :to="'/news/messige/list/?id='+itrem.id">{{itrem.text}} query</router-link>
+        <Button class="btn" @click="push(itrem.id)">push栈查看</Button>
+        <button class="btn" @click="replace(itrem.id)">repaace查看</button>
+        </li>
     </ul>
+    <Button class="btn btn-default" @click="$router.back()">返回</Button>
+    <p>编程式导航(使用JS实现跳转)</p>
     <hr>
     <router-view></router-view>
 </div>
 </template>
 
 <script>
+//window.location.href=url;
+//window.location=url;
 export default {
     data(){
         return{
@@ -35,7 +42,15 @@ export default {
             ]
         this.items=items;
     },1000)
- }
+ },
+    methods:{
+        push(id){
+            this.$router.push("/news/messige/list/"+id);
+        },
+        replace(id){
+            this.$router.replace("/news/messige/list/"+id);
+        },
+    }
 }
 </script>
 
